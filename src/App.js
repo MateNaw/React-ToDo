addae9b3999705246ab1d936944e269a91607b86
MateNaw <49524385+MateNaw@users.noreply.github.com>
2020-03-08 13:14:17 +0100

    showing vs code git setuip

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import TodoItem from "./components/TodoItem";
-
+//showing vs code setup
 class App extends Component {
   render() {
     return (
