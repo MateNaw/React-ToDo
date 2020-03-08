eb208e331236547a81b9278017cea0baec71ddca
MateNaw <49524385+MateNaw@users.noreply.github.com>
2020-03-08 15:11:01 +0100

    ClearList + handleDelete

--- src/App.js
+++ src/App.js
@@ -26,22 +26,24 @@ class App extends Component {
       title: this.state.item
     };
     const updatedItems = [...this.state.items, newItem];
-    this.setState(
-      {
-        items: updatedItems,
-        item: "",
-        id: v1(),
-        editItem: false
-      },
-      () => console.log(this.state)
-    );
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: v1(),
+      editItem: false
+    });
   };
 
   clearList = () => {
-    console.log("clear list method");
+    this.setState({
+      items: []
+    });
   };
   handleDelete = id => {
-    console.log(`handle delete ${id}`);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    });
   };
   handleEdit = id => {
     console.log(`handle edit ${id}`);
