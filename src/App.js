308498a59b5d12b01500d75e608130408739fea1
MateNaw <49524385+MateNaw@users.noreply.github.com>
2020-03-08 13:07:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoItem from "./components/TodoItem";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
